85c83ff7fc04ad1046a35679cdf1a7734263e289
Mike Nezzer <57691487+mnezz1131@users.noreply.github.com>
2020-06-29 20:38:43 -0500

    pics rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import SearchBar from "./SearchBar"
+import unsplash from "../api/unsplash";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
-const App = () => {
-return (
-   <div className="ui container" style={{marginTop: "10px"}}>
-     <SearchBar />
-   </div>
-  );
-};
+class App extends React.Component {
+// //OLD WAY
+//   onSearchSubmit(term) {
+//     console.log(term) 
+//     // FIRST ARGUMENT IS THE URL, THE {SECOND ARGUMENT IS THE REST OF IT PARAMS AND HEADERS}
+//     axios.get ('https://api.unsplash.com/search/photos', {
+//       // QUERY TERM REFERS TO THE TERM IN LINE 8 onSearchSubmit(term)
+//         params: {query: term},
+//         headers:{
+//           Authorization: 'Client-ID ANWRTldr9QUUMlMfZhRCeU97Qp1IVnbsKPEB-u_qMUg'
+//         }
+//           }).then((response) => {
+//             console.log(response.data.results)
+//           })
+//   }
+state = {images:[]};
+//New way
+  onSearchSubmit = async (term) => {
+    console.log(term) 
+ 
+    // FIRST ARGUMENT IS THE URL, THE {SECOND ARGUMENT IS THE REST OF IT PARAMS AND HEADERS}
+    const response = await unsplash.get ('/search/photos', {
+      // QUERY TERM REFERS TO THE TERM IN LINE 8 onSearchSubmit(term)
+        params: {query: term},
+        
+          });
+            console.log(response.data.results)
+          this.setState ({images: response.data.results});
+  }
+
+  render () {
+    return(
+      <div className="ui container" style={{marginTop: "10px"}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      <ImageList images={this.state.images} />
+      </div>
+      );
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,43 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange (event) {
-    // event.target.value
-    console.log(event.target.value);
-    }
+  // controlled 
+  state = {term:""};
+  
+  
+    // uncontrolled event handler 
+  // onInputChange (event) {
+  //   // event.target.value
+  //   console.log(event.target.value);
+  //   }
 
-    // onInputClick () {
+  //   // onInputClick () {
     //     console.log ("input was clicked")
     // }
 
+//Do not want form to submit itself automatically and refresh the page
 
-
+onFormSubmit = (event) => {
+  event.preventDefault ();
+  // reference to function that got passed in on App.js line13
+  //class based component reference props with this.props
+  this.props.onSubmit(this.state.term)
+}
 
     render () {
        return( 
        <div className="ui segment">
-           <form className="ui form">
+           <form onSubmit = {this.onFormSubmit} className="ui form">
              <div className="field">
                 <label >Image Search</label>
          {/* onInputChange is a callback function, don't use onInputchange () because it will call the the function everytime it's rendered. so we are  passing a reference to the funcion to the input element.  Don't use () when using a callback to an event handler 
          Property name is onChange
          */}
-                 <input type ="text" 
-                //   onClick ={this.onInputClick} 
-                  onChange={this.onInputChange} /> 
+                 <input 
+                 type ="text" 
+                 value={this.state.term}
+                 onChange={e => this.setState({ term: e.target.value })} 
+                 /> 
            
              </div>  
            </form>
